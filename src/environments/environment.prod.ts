f6c3bde48582d170e2e137b43faa475c58b24636
Avinash <aka.avinash.singh@gmail.com>
2025-08-21 17:38:56 +0530

    Aded secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   GOOGLE_API_KEY: "AIzaSyDb7zoRltrfSat3aq7o9EYF5VV4hEKwNUE",
   X_API_KEY: "f68dd92ab05fa026457651a34b7ef4b18cf289a5",
   BASE_URL: "https://nectarplus.health/",
-
+  ADMIN_SECRET_KEY:"super-secret-key-here",
   SURGERY_IMAGE_URL:
     "https://nector-prod.s3.ap-south-1.amazonaws.com/193853b0-673a-11ee-b72e-f758ecf0cdcc-Surgery%20Desktop%20Banner.png",
   SURGERY_ICON_IMAGE_URL:
